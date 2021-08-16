efe57ef3a5b694b374c3d8ecb96b17daa696396b
Chase <cabbott93@gmail.com>
2021-08-16 11:04:25 -0700

    change state back to array

--- src/containers/WeatherContainer.tsx
+++ src/containers/WeatherContainer.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useWeather from '../state/useWeather';
 
 
 export default function WeatherContainer(){
   const { currentWeather } = useWeather();
 
+  useEffect(() => {
+    console.log(currentWeather);
+  });
+
   return (
     <div className="m-8">
       <h3> Current Weather in Carmel-By-The-Sea </h3>
       <figure className="flex m-4 justify-between items-center">
         <div>
-          <figcaption> {currentWeather['Temperature']['Imperial'].Value}F</figcaption>
-          <figcaption> {currentWeather['Temperature']['Metric'].Value}C </figcaption>
+          <figcaption> {currentWeather[0].Temperature.Imperial.Value}F</figcaption>
+          <figcaption> {currentWeather[0].Temperature.Metric.Value}C </figcaption>
         </div>
-        <figcaption> {currentWeather['WeatherText']}</figcaption>
-        <img src={`./${currentWeather['WeatherIcon']}-s.png`} alt={currentWeather['WeatherText']}></img>
+        <figcaption> {currentWeather[0].WeatherText}</figcaption>
+        <img src={`./${currentWeather[0].WeatherIcon}-s.png`} alt={currentWeather['WeatherText']}></img>
       </figure>
     </div>
   );
