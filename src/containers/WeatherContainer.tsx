5e8378cabd257c450b4069bf3d7555aa748ef4e4
Chase <cabbott93@gmail.com>
2021-08-17 09:36:03 -0700

    add accuweather logo

--- src/containers/WeatherContainer.tsx
+++ src/containers/WeatherContainer.tsx
@@ -1,19 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import useWeather from '../state/useWeather';
 
 
 export default function WeatherContainer(){
   const { currentWeather } = useWeather();
 
-  useEffect(() => {
-    console.log(currentWeather);
-  });
-
   return (<>
     {currentWeather[0] ?
-      <div className="m-8">
+      <div className="m-8 w-6/12 flex flex-col items-center">
         <h3> Current Weather in Carmel-By-The-Sea </h3>
-        <figure className="flex m-4 justify-between items-center">
+        <figure className="flex m-4 justify-between items-center w-full">
+          <img src='./AccuWeather.png' alt="accuweather" className="w-2/12"/>
           <div>
             <figcaption> {currentWeather[0].Temperature.Imperial.Value}F</figcaption>
             <figcaption> {currentWeather[0].Temperature.Metric.Value}C </figcaption>
